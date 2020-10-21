bc6f3899879ff821b3f085bc299ac8e9c12cdcd8
Chris Lyon <chris@cplyon.ca>
2020-10-21 10:27:54 -0700

    Is Binary Tree Symmetric
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/627/

---
+++ trees/is_symmetric.kt
@@ -0,0 +1,48 @@
+fun main() {
+
+    var root: TreeNode? = TreeNode(1)
+    root?.left = TreeNode(2)
+    root?.right = TreeNode(2)
+    root?.left?.left = TreeNode(3)
+    root?.left?.right = TreeNode(4)
+    root?.right?.left = TreeNode(4)
+    root?.right?.right = TreeNode(3)
+
+
+    println(isSymmetric(root))
+}
+
+fun isSymmetric(root: TreeNode?): Boolean {
+    if (root == null) {
+        return true
+    }
+
+   // flipChildren(root.left)
+    return areEqual(root.left, root.right)
+}
+
+fun areEqual(left: TreeNode?, right: TreeNode?) : Boolean {
+    // both nodes are null
+    if (left == null && right == null) {
+        return true
+    }
+
+    // only one node is null
+    if (left == null || right == null) {
+        return false
+    }
+
+    // values are not equal
+    if (left.`val` != right.`val`) {
+        return false
+    }
+
+    // recurse down a level
+    return areEqual(left.left, right.right) && areEqual(left.right, right.left)
+}
+
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
