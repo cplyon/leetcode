2bb48daa7dea44b739e5efd23ca289cf58356569
Chris Lyon <chris@cplyon.ca>
2020-11-23 15:41:32 -0800

    Sort Colors
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/798/

---
+++ sorting_and_searching/sort_colors.kt
@@ -0,0 +1,51 @@
+fun main() {
+    val s = Solution()
+    val nums = intArrayOf(2,0)
+    s.printArray(nums)
+    println()
+    s.sortColors(nums)
+    s.printArray(nums)
+}
+
+class Solution {
+    fun sortColors(nums: IntArray): Unit {
+
+        var head = 0
+        var tail = nums.lastIndex
+        if (tail <= head) {
+            // empty list or single element
+            return
+        }
+
+        var cur = head
+        while (cur <= tail) {
+            if (nums[cur] == 2) {
+                if (nums[tail] != 2) {
+                    swap(nums, cur, tail)
+                }
+                tail--
+            } else if (nums[cur] == 0) {
+                if (cur > head && nums[head] != 0) {
+                    swap(nums, cur, head)
+                }
+                head++
+                cur = maxOf(cur, head)
+            } else {
+                cur++
+            }
+
+            printArray(nums)
+            println(" $head $tail $cur")
+        }
+    }
+
+    fun swap(nums: IntArray, i: Int, j: Int) {
+        val temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+    }
+
+    fun printArray(array: IntArray) = array.forEach {
+        print("$it ")
+    }
+}
