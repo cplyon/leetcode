ae03088f89bffa18a227f04121890ce8dce0e155
Chris Lyon <chris@cplyon.ca>
2020-11-28 13:13:14 -0800

    Find First and Last Position of Element in Sorted Array
    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/

---
+++ sorting_and_searching/search_for_a_range.kt
@@ -0,0 +1,95 @@
+fun main() {
+    val s = Solution()
+    val nums = intArrayOf(0,0,0,1,1,2,2,2,2,3,3,4,4,4,4,5,64)
+    val target = 4
+    println(s._searchRange(nums, target).toList())
+    println(s.searchRange(nums, target).toList())
+}
+
+class Solution {
+
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+        // O(nlogn)
+
+        val range = intArrayOf(-1,-1)
+        if (nums.isEmpty()) {
+            return range
+        }
+
+        val firstFound = binarySearch(nums, 0, nums.size-1, target)
+        if (firstFound < 0) {
+            return range
+        }
+        range[0] = firstFound
+        range[1] = firstFound
+
+        var leftResult = firstFound
+        while (leftResult >= 0) {
+            leftResult = binarySearch(nums, 0, leftResult-1, target)
+            if (leftResult >= 0) {
+                range[0] = leftResult
+            }
+        }
+        var rightResult = firstFound
+        while (rightResult >= 0) {
+            rightResult = binarySearch(nums, rightResult+1, nums.size, target)
+            if (rightResult >= 0) {
+                range[1] = rightResult
+            }
+        }
+        return range
+    }
+
+    fun binarySearch(nums: IntArray, leftMost: Int, rightMost: Int, target: Int): Int {
+        var left = leftMost
+        var right = rightMost
+        while (left <= right) {
+            var mid = (left+right)/2
+            if (mid >= nums.size) {
+                return -1
+            }
+            if (nums[mid] > target) {
+                right = mid - 1
+            } else if (nums[mid] < target) {
+                left = mid + 1
+            } else {
+                return mid
+            }
+        }
+        return -1
+    }
+
+    fun _searchRange(nums: IntArray, target: Int): IntArray {
+        // worst case, O(n)
+
+        val range = intArrayOf(-1,-1)
+        if (nums.isEmpty()) {
+            return range
+        }
+
+        var left = 0
+        var right = nums.size - 1
+        while (left <= right) {
+            var mid = (left+right)/2
+            if (nums[mid] > target) {
+                right = mid - 1
+            } else if (nums[mid] < target) {
+                left = mid + 1
+            } else {
+                var leftRange = mid
+                while (leftRange >= 0 && nums[leftRange] == target) {
+                    range[0] = leftRange
+                    leftRange--
+                }
+                var rightRange = mid
+                while (rightRange < nums.size && nums[rightRange] == target) {
+                    range[1] = rightRange
+                    rightRange++
+                }
+                return range
+            }
+        }
+
+        return range
+    }
+}
