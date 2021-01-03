25b977a4eab4b45433e21d4a3c4474d0645f0317
Chris Lyon <chris@cplyon.ca>
2021-01-02 16:48:03 -0800

    Optimize Frog Jump
    Don't need to keep track of the destination stone.

--- dynamic_programming/frog_jump.py
+++ dynamic_programming/frog_jump.py
@@ -7,30 +7,32 @@ class Solution:
         if not stones or len(stones) < 1 or stones[0] != 0:
             return False
 
-        # Map stones to destination stones and their costs.
-        # Use a set to avoid duplicates.
+        # Map stones to the costs of jumps that can be made.
         costs = {}
         # From the stone '0', we can only go to '1' at a cost of 1
-        costs[0] = {(1, 1)}
+        costs[0] = [1]
         for stone in stones[1:]:
             costs[stone] = set()
 
         # Map stones to their sources. Use a set to avoid duplicates.
         sources = DefaultDict(set)
+        # No way to get to stone '0'
         sources[0] = set()
         # The only way to get to the stone '1' is from stone '0'
         sources[1] = {0}
 
         for stone in stones[1:]:
             for src in sources[stone]:
-                for dest, cost in costs[src]:
+                for cost in costs[src]:
+                    dest = cost + src
                     if dest == stone:
                         for k in range(-1, 2):
                             if dest+cost+k == stones[-1]:
                                 # we found a path to the final stone!
                                 return True
-                            if dest+cost+k != dest and dest+cost+k in costs:
-                                costs[stone].add((dest+cost+k, cost+k))
+                            if dest+cost+k != dest:
+                                # don't add duplicates
+                                costs[stone].add(cost+k)
                                 sources[dest+cost+k].add(stone)
 
         # no path to the final stone
@@ -40,7 +42,7 @@ class Solution:
 if __name__ == "__main__":
     s = Solution()
     stones = [0,1,3,5,6,8,12,17]
-    #stones = [0,1,3,6,10,15,21,28]
+    #stones = [0,1,3,6,10,15,21,29]
     #stones = [0,1,2,3,4,5]
     #stones = [0,1,2,3,4,8,9,11]
     print(s.canCross(stones))
