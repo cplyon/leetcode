d67fc889f63724f404a6e82d0ecff08c43bc96d1
Chris Lyon <chris@cplyon.ca>
2021-01-03 16:08:31 -0800

    Isomorphic Strings
    https://leetcode.com/problems/isomorphic-strings

---
+++ strings/is_isomorphic.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+class Solution:
+
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        if len(s) != len(t):
+            return False
+
+        s_letters = {}
+        t_letters = {}
+
+        for i in range(len(s)):
+
+            if t[i] in t_letters and t_letters[t[i]] != s[i]:
+                return False
+
+            if s[i] in s_letters and s_letters[s[i]] != t[i]:
+                return False
+
+            t_letters[t[i]] = s[i]
+            s_letters[s[i]] = t[i]
+
+        return True
+
+if __name__ == "__main__":
+    solution = Solution()
+    s = "aa"
+    t = "ab"
+    print(solution.isIsomorphic(s, t))
