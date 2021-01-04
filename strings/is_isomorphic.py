baab82269cedf52a0ea68a6f9e8fb6ac03ece5b6
Chris Lyon <chris@cplyon.ca>
2021-01-03 22:30:19 -0800

    Update Is Isomorphic
    More succint code

--- strings/is_isomorphic.py
+++ strings/is_isomorphic.py
@@ -14,7 +14,6 @@ class Solution:
         t_letters = {}
 
         for i in range(len(s)):
-<<<<<<< HEAD
 
             if t[i] in t_letters and t_letters[t[i]] != s[i]:
                 return False
@@ -24,29 +23,11 @@ class Solution:
 
             t_letters[t[i]] = s[i]
             s_letters[s[i]] = t[i]
-=======
-            if t[i] not in t_letters:
-                t_letters[t[i]] = s[i]
-            else:
-                if t_letters[t[i]] != s[i]:
-                    return False
-
-            if s[i] not in s_letters:
-                s_letters[s[i]] = t[i]
-            else:
-                if s_letters[s[i]] != t[i]:
-                    return False
->>>>>>> 199a70c03ce28dd23c1e7b2de3cc3279ef9cc4f6
 
         return True
 
 if __name__ == "__main__":
     solution = Solution()
-<<<<<<< HEAD
-    s = "aa"
-    t = "ab"
-=======
     s = "ab"
     t = "aa"
->>>>>>> 199a70c03ce28dd23c1e7b2de3cc3279ef9cc4f6
     print(solution.isIsomorphic(s, t))
