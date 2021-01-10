169e23fa0f7c1544968990483ffda7d0e6c7b585
Chris Lyon <chris@cplyon.ca>
2021-01-10 13:16:18 -0800

    Missing Number
    https://leetcode.com/problems/missing-number/

--- other/missing_number.py
+++ other/missing_number.py
@@ -4,14 +4,25 @@ class Solution:
     def missingNumber(self, nums: list[int]) -> int:
         # find missing number by subtracting the sum of nums
         # from the expected sum
+        # O(n+n) => O(n)
         return sum([i for i in range(len(nums)+1)]) - sum(nums)
 
     def missingNumber2(self, nums: list[int]) -> int:
         # find missing number by subtracting the sum of nums
         # from the mathematically-derived expected sum
         # (n*(n+1))/2 = sum(0..n)
+        # O(n)
         n = len(nums)
         return (n*(n+1))//2 - sum(nums)
+
+    def missingNumber3(self, nums: list[int]) -> int:
+        # find missing number by subtracting each values in nums
+        # from each value from 0 to n
+        # O(n)
+        missing_number = 0
+        for i in range(len(nums)):
+            missing_number += i - nums[i]
+        return missing_number + len(nums)
 
 
 if __name__ == "__main__":
@@ -19,3 +30,4 @@ if __name__ == "__main__":
     nums = [0, 1, 2, 3, 4, 6]
     print(s.missingNumber(nums))
     print(s.missingNumber2(nums))
+    print(s.missingNumber3(nums))
