33461312fa2c6f28f7b142dc559a70c9c5743e1d
Chris Lyon <chris@cplyon.ca>
2021-01-14 20:28:55 -0800

    Tweaks to Median of 2 Sorted Lists

--- arrays/median_of_two_sorted_arrays.py
+++ arrays/median_of_two_sorted_arrays.py
@@ -4,13 +4,6 @@ class Solution:
 
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
         # O(n+m), since we iterate both lists
-        if not nums1 and not nums2:
-            return 0.0
-        if nums1 and not nums2:
-            return self.findMedian(nums1)
-        if nums2 and not nums1:
-            return self.findMedian(nums2)
-
         return self.findMedian(self.merge(nums1, nums2))
 
     def merge(self, nums1: list[int], nums2: list[int]) -> list[int]:
@@ -41,15 +34,16 @@ class Solution:
         return merged
 
     def findMedian(self, nums: list[int]) -> float:
+        mid = len(nums)//2
         if not nums:
             return 0.0
         if len(nums) % 2 != 0:
-            return float(nums[len(nums)//2])
-        return (nums[len(nums)//2] + nums[len(nums)//2-1])/2
+            return float(nums[mid])
+        return (nums[mid] + nums[mid-1])/2
 
 
 if __name__ == "__main__":
     s = Solution()
-    nums1 = [1, 3]
-    nums2 = [2, 4]
+    nums1 = []
+    nums2 = [4]
     print(s.findMedianSortedArrays(nums1, nums2))
