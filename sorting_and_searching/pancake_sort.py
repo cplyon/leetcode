3ab7e85aff2011dc59e1fda9e7647904f088448f
Chris Lyon <chris@cplyon.ca>
2021-02-04 10:27:43 -0800

    Pancacke Sorting
    https://leetcode.com/problems/pancake-sorting/

---
+++ sorting_and_searching/pancake_sort.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+class Solution:
+    def pancakeSort(self, arr: list[int]):
+        #if not arr or len(arr) < 2:
+        #    return arr
+
+        def flip(arr, k):
+            """
+            if 1 >= k > len(arr):
+                return arr
+            if not arr:
+                return arr
+            """
+            stack = []
+            for i in range(k):
+                stack.append(arr.pop(0))
+            while len(stack) > 0:
+                arr.insert(0, stack.pop(0))
+
+
+        max_val = len(arr)
+        result = []
+        expected = sorted(arr)
+        while arr != expected:
+            for i,n in enumerate(arr):
+                if n == max_val:
+                    flip(arr, i+1)
+                    result.append(i+1)
+                    flip(arr, max_val)
+                    result.append(max_val)
+                    max_val -= 1
+
+
+        return result
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    arr = [3,2,4,1]
+    print(s.pancakeSort(arr))
