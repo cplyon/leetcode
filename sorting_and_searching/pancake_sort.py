cca274cff1865c1726134d7faa0998ac697148bb
Chris Lyon <chris@cplyon.ca>
2021-02-04 10:30:42 -0800

    Improve Pancake Sort solution

--- sorting_and_searching/pancake_sort.py
+++ sorting_and_searching/pancake_sort.py
@@ -2,35 +2,30 @@
 
 class Solution:
     def pancakeSort(self, arr: list[int]):
-        #if not arr or len(arr) < 2:
-        #    return arr
+        if not arr or len(arr) < 2:
+            return arr
 
         def flip(arr, k):
-            """
             if 1 >= k > len(arr):
                 return arr
             if not arr:
                 return arr
-            """
-            stack = []
-            for i in range(k):
-                stack.append(arr.pop(0))
-            while len(stack) > 0:
-                arr.insert(0, stack.pop(0))
 
+            temp = arr[:k]
+            temp.reverse()
+            arr[:k] = temp
 
         max_val = len(arr)
         result = []
         expected = sorted(arr)
         while arr != expected:
-            for i,n in enumerate(arr):
+            for i, n in enumerate(arr):
                 if n == max_val:
                     flip(arr, i+1)
                     result.append(i+1)
                     flip(arr, max_val)
                     result.append(max_val)
                     max_val -= 1
-
 
         return result
 
